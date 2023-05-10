601c1f0ffc33652a73c0de5884df6029750a1054
Guilherme Augusto Fernandes <69806054+guigutox@users.noreply.github.com>
2023-05-10 10:55:25 -0300

    Continuação triggers

--- Prova_Triggers.sql
+++ Prova_Triggers.sql
@@ -75,3 +75,131 @@ create or replace trigger num_empregados
 after insert or delete on empregado
 for each row
 execute function empregadosNum();
+
+
+Estrutura da função
+Create function nome_funcao(parametro tipo)
+returns tipo_retorno as
+$$
+--VARIAVEL GLOBAL
+Declare
+var tipo
+
+--VARIAVEL DE REGISTRO que recebe varias colunas de uma tabela do tipo record
+registro record; 
+
+
+BEGIN
+	--Declarar variavel
+	nome_var tipo := valor;
+
+	--Copiar tipo
+	var varAcopiar%type -- Vai copiar o tipo da varivel
+
+	--Printar no console
+	raise notice 'STRING';
+
+	--Jogar valor de consulta numa variavel
+	select max(NUM) from tabela into var -- VARIAVEL var recebe o maior numero da coluna num
+
+	--looping
+	for var in select atributos from tabela loop
+
+	end loop;
+
+	--
+
+
+	RETURN valor;
+END;
+$$ language plpgsql
+
+
+
+--É possivel retornar uma tabela interia 
+create or replace function retorna_pessoa() 
+returns SETOF pessoa --SETOF indica que a função retorna um conjunto de registros de tipo pessoa
+as $$
+begin
+	--Return query indica que ira retornara o resultado de uma query completa
+	return query select * from pessoa;
+End;
+$$ language 'plpgsql';
+
+create or replace function debuga()
+returns void as 
+$$
+	begin
+		raise debug 'Valor x = %', x; -- Raise debug escreve em um log
+		raise notice 'Valor x = %', x; -- Raise notice manda uma mensagem e continua a execução
+		raise exception 'Valor x = %', x; --Escreve no logo e termina a execução naquela parte
+	end;
+$$
+
+
+--Estrutura do if
+
+if num > then
+
+elsif num < 0 then
+
+else 
+
+end if;
+
+
+--Estrutura do for
+declare
+contador int := 1;
+
+for contador in 1..5 loop
+end loop
+
+--For para percorrer linhas da tabela
+Declare 
+	linha record; -- record tipo generico
+BEGIN
+	For linha in select * from clientes loop
+
+	raise notice 'Cliente: %', linha.nome;
+	END loop;
+
+END;
+
+
+-- Estrutura do while
+while contador <= 5 loop
+	contador := contador + 1;
+end loop;
+
+--Estrutura do case
+
+case
+	when opcao = 1 then
+	when opcao = 2 then
+	when opcao = 3 then
+	else
+end case;
+
+
+--TRIGGER ESTRUTURA
+
+create or replace TRIGGER nome (before ou after)
+(Evento insert, update ou delete)
+on tabela for each (row ou statement)
+execute procedure funcao(argumentos);
+
+-- BEFORE = a trigger é chamada antes de ocorrer o evento definido geralmente definido antes dos dados serem atualizados ou inseridos
+
+-- AFTER = a trigger é chamada depois do evento ocorrer  verificar integridade com outras tabelas ou propagar atualizações
+
+--Insert tem acesso ao new apenas, update tem acesso ao new e old, delete tem acesso ao old apenas
+
+-- TG OP Permite descobrir qual a ação e devolve o que esta acontecendo devolve em texto insert, update ou delete
+
+--Return do before
+--Return new para inset e update
+-- Return old para delete
+-- Para cancelar a operação return null
+
+--After o tipo retornado é ignorado então deve ser null
